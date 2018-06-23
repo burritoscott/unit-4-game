a105f5e76532ee98d3edd11503a4fccc0d59d761
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-06-23 09:13:35 -0700

    begun if else statement to add the score values together and score still does not appear in the score span

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 
 var wins = 0;
 var losses = 0;
-var randomNum, greenNum, pinkNum, purpleNum, yellowNum;
+var randomNum, greenNum, pinkNum, purpleNum, yellowNum, randomNumDis, yourScore;
 
 //generate a random number to guess and display it btwn 19-120
 function getRandomNum(min, max) {
     randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    var randomNumDis = $("#randomNumber");
+    randomNumDis = $("#randomNumber");
     randomNumDis.text(randomNum);
 
 //generate a random number for each jewel btwn 1-12
@@ -18,7 +18,20 @@ function getRandomNum(min, max) {
 
 //make images clickable using their individual IDs
 
-    
+    $(".jewels").on("click", function(){
+        yourScore = $(this).val(Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+        yourScoreDis = $("#score");
+        yourScoreDis.text(yourScore);
+        
+    });
+
+    if (yourScore == randomNum){
+        wins ++;
+    }else if(yourScore > randomNum){
+        losses ++;
+    }else{
+        //add value of next click to current score
+    }
 }
 
 // //create a counter for wins and losses
