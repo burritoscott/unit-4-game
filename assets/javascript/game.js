a98484ffab132e13c2f86802b3bcc2134a777c79
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-06-22 21:20:27 -0700

    have random numbers generating and number to guess appends to the document

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,26 @@
-//generate a random number to guess
-//make images clickable using their individual IDs
-//create a counter for wins and losses
-//
 
 var wins = 0;
 var losses = 0;
+var randomNum, greenNum, pinkNum, purpleNum, yellowNum;
+
+//generate a random number to guess and display it btwn 19-120
+function getRandomNum(min, max) {
+    randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    var randomNumDis = $("#randomNumber");
+    randomNumDis.text(randomNum);
+
+//generate a random number for each jewel btwn 1-12
+
+    greenNum = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    pinkNum = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    purpleNum = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    yellowNum = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
+//make images clickable using their individual IDs
+
+    
+}
+
+// //create a counter for wins and losses
+// //
+getRandomNum();
